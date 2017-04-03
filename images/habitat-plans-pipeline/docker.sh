b7bd86122a938b3a7e831e1f8335cd33631b3e1f
Justin Carter <justin@starkandwayne.com>
2017-04-03 11:26:50 +0200

    Formatting

--- images/habitat-plans-pipeline/docker.sh
+++ images/habitat-plans-pipeline/docker.sh
@@ -36,50 +36,50 @@ sanitize_cgroups() {
     ln -s "$mountpoint" "/sys/fs/cgroup/$sys"
   fi
 done
-                                                                                                                                }
+}
 
-                                                                                                                                start_docker() {
-                                                                                                                                  mkdir -p /var/log
-                                                                                                                                  mkdir -p /var/run
+start_docker() {
+  mkdir -p /var/log
+  mkdir -p /var/run
 
-                                                                                                                                  sanitize_cgroups
+  sanitize_cgroups
 
-                                                                                                                                  # check for /proc/sys being mounted readonly, as systemd does
-                                                                                                                                  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
-                                                                                                                                    mount -o remount,rw /proc/sys
-                                                                                                                                  fi
+  # check for /proc/sys being mounted readonly, as systemd does
+  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
+    mount -o remount,rw /proc/sys
+  fi
 
-                                                                                                                                  local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
-                                                                                                                                  local server_args="--mtu ${mtu}"
-                                                                                                                                  local registry=""
+  local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
+  local server_args="--mtu ${mtu}"
+  local registry=""
 
-                                                                                                                                  for registry in $1; do
-                                                                                                                                    server_args="${server_args} --insecure-registry ${registry}"
-                                                                                                                                  done
+  for registry in $1; do
+    server_args="${server_args} --insecure-registry ${registry}"
+  done
 
-                                                                                                                                  if [ -n "$2" ]; then
-                                                                                                                                    server_args="${server_args} --registry-mirror=$2"
-                                                                                                                                  fi
+  if [ -n "$2" ]; then
+    server_args="${server_args} --registry-mirror=$2"
+  fi
 
-                                                                                                                                  docker daemon ${server_args} >/tmp/docker.log 2>&1 &
-                                                                                                                                  echo $! > /tmp/docker.pid
+  docker daemon ${server_args} >/tmp/docker.log 2>&1 &
+  echo $! > /tmp/docker.pid
 
-                                                                                                                                  trap stop_docker EXIT
+  trap stop_docker EXIT
 
-                                                                                                                                  sleep 1
+  sleep 1
 
-                                                                                                                                  until docker info >/dev/null 2>&1; do
-                                                                                                                                    echo waiting for docker to come up...
-                                                                                                                                    sleep 1
-                                                                                                                                  done
-                                                                                                                                }
+  until docker info >/dev/null 2>&1; do
+    echo waiting for docker to come up...
+    sleep 1
+  done
+}
 
-                                                                                                                                stop_docker() {
-                                                                                                                                  local pid=$(cat /tmp/docker.pid)
-                                                                                                                                  if [ -z "$pid" ]; then
-                                                                                                                                    return 0
-                                                                                                                                  fi
+stop_docker() {
+  local pid=$(cat /tmp/docker.pid)
+  if [ -z "$pid" ]; then
+    return 0
+  fi
 
-                                                                                                                                  kill -TERM $pid
-                                                                                                                                  wait $pid
-                                                                                                                                }
+  kill -TERM $pid
+  wait $pid
+}
