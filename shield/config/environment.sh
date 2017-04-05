6d86c5f45b840eb847bd4b8d09a4287bb6d12d52
Justin Carter <justin@starkandwayne.com>
2017-04-05 09:11:50 +0200

    [shield] init hook sets up db

---
+++ shield/config/environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+{{ #each bind.database.members }}
+PGPORT="{{cfg.port}}"
+PGHOST="{{sys.ip}}"
+PG_SUPERUSER="{{cfg.superuser_name}}"
+PG_SUPERUSER_PASSWORD="{{cfg.superuser_password}}"
+PG_SUPERUSER_URI="postgres://{{cfg.superuser_name}}:{{cfg.superuser_password}}@{{sys.ip}}:{{cfg.port}}/postgres"
+{{ /each }}
