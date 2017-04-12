ce2ce615015f33b65d34707b9af32ea0cd882aed
Justin Carter <justin@starkandwayne.com>
2017-04-12 09:37:39 +0200

    Update redis and move run cmd to hook

--- redis/plan.sh
+++ redis/plan.sh
@@ -1,16 +1,15 @@
 pkg_name=redis
-pkg_origin=core
-pkg_version=3.2.4
+pkg_origin=starkandwayne
+pkg_version=3.2.8
 pkg_description="Persistent key-value database, with built-in net interface"
 pkg_upstream_url=http://redis.io/
 pkg_license=('BSD-3-Clause')
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_maintainer="Justin Carter <justin@starkandwayne.com>"
 pkg_source=http://download.redis.io/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=2ad042c5a6c508223adeb9c91c6b1ae091394b4026f73997281e28914c9369f1
+pkg_shasum=61b373c23d18e6cc752a69d5ab7f676c6216dc2853e46750a8c4ed791d68482c
 pkg_bin_dirs=(bin)
 pkg_build_deps=(core/make core/gcc)
 pkg_deps=(core/glibc)
-pkg_svc_run="redis-server $pkg_svc_config_path/redis.config"
 pkg_exports=(
   [port]=port
 )
