0384880e707c9a52a53a5d333de5312af05b3d13
ramonskie <ramonmakkelie@gmail.com>
2017-04-14 18:11:25 +0200

    extract and copy tarball to public_html

--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -6,11 +6,13 @@ pkg_origin=starkandwayne
 pkg_version="4.7.3"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 #pkg_license=('Apache-2.0')
-#pkg_filename="${pkg_name}-${pkg_version}"
+# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
 
 #if we want the latest source, a non empty string is required, but we are building locally
 pkg_source="https://wordpress.org/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="e715069fdab049ec668cd74e57cabcb23583dc4a84c5e330c430cbe8998f6d89"
+
+source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}
 
 # Optional.
 # The resulting filename for the download, typically constructed from the
@@ -21,6 +23,8 @@ pkg_exports=()
 pkg_exposes=()
 
 
+
+
 do_build(){
   return 0
 }
@@ -29,24 +33,23 @@ do_build(){
 # environment variables have been set, so you can use this callback to perform
 # any actions before the package starts building, such as exporting variables,
 # adding symlinks, and so on.
-do_prepare() {
-  return 0
-}
-
-do_check() {
-  return 0
-}
+# do_prepare() {
+#   return 0
+# }
+#
+# do_check() {
+#   return 0
+# }
 
 do_install() {
-  attach
-  mkdir $pkg_prefix/public_html
-  cp $pkg_filename $pkg_prefix/public_html
+  # mkdir $pkg_prefix/public_html
+  cp -r "$source_dir" $pkg_prefix/public_html/
 }
 
-do_strip() {
-  do_default_strip
-}
-
-do_end() {
-  return 0
-}
+# do_strip() {
+#   do_default_strip
+# }
+#
+# do_end() {
+#   return 0
+# }
