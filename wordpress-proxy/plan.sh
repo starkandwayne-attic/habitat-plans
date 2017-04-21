3c4ab3be95306aa51eaa746c8bec0d1b24fdeac4
ramonskie <ramonmakkelie@gmail.com>
2017-04-21 10:24:50 +0200

    cleanup

--- wordpress-proxy/plan.sh
+++ wordpress-proxy/plan.sh
@@ -1,22 +1,19 @@
 # This file is the heart of your application's habitat.
 # See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
 
-# Required.
-# Sets the name of the package. This will be used in along with `pkg_origin`,
-# and `pkg_version` to define the fully-qualified package name, which determines
-# where the package is installed to on disk, how it is referred to in package
-# metadata, and so on.
 pkg_name=wordpress-proxy
 pkg_origin=starkandwayne
-
+pkg_maintainer="Ramon Makkelie <makkelie@starkandwayne.com>"
+pkg_license=('Apache-2.0')
 pkg_version=undefined
 pkg_source=nosuchfile.tar.xz
 
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
 
 pkg_deps=(core/nginx)
 
-pkg_svc_user=root
-pkg_svc_group=$pkg_svc_user
+
 
 do_begin() {
   return 0
--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -4,23 +4,16 @@
 pkg_name=wordpress
 pkg_origin=starkandwayne
 pkg_version="4.7.3"
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-#pkg_license=('Apache-2.0')
-# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-
-#if we want the latest source, a non empty string is required, but we are building locally
+pkg_maintainer="Ramon Makkelie <makkelie@starkandwayne.com>"
+pkg_license=('Apache-2.0')
 pkg_source="https://wordpress.org/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="e715069fdab049ec668cd74e57cabcb23583dc4a84c5e330c430cbe8998f6d89"
 
 source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}
 
-#DO NOT USE
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
 
-# Optional.
-# The resulting filename for the download, typically constructed from the
-# pkg_name and pkg_version values.
 pkg_deps=(core/php core/curl starkandwayne/wordpress-proxy)
 
 pkg_exports=()
@@ -31,23 +24,9 @@ pkg_exposes=()
 # )
 
 
-
-
 do_build(){
   return 0
 }
-# There is no default implementation of this callback. At this point in the
-# build process, the tarball source has been downloaded, unpacked, and the build
-# environment variables have been set, so you can use this callback to perform
-# any actions before the package starts building, such as exporting variables,
-# adding symlinks, and so on.
-# do_prepare() {
-#   return 0
-# }
-#
-# do_check() {
-#   return 0
-# }
 
 do_install() {
   # mkdir $pkg_prefix/public_html
@@ -56,11 +35,3 @@ do_install() {
   # rename file so we can use fpm default
   cp "$(pkg_path_for php)/etc/php-fpm.d/www.conf.default" "$(pkg_path_for php)/etc/php-fpm.d/www.conf"
 }
-
-# do_strip() {
-#   do_default_strip
-# }
-#
-# do_end() {
-#   return 0
-# }
