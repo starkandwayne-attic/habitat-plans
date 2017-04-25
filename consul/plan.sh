70b4249b83b1fcb3567ddc29a609825dac8c6eb7
Bill Chapman <billchapman@starkandwayne.com>
2017-04-25 12:17:04 -0400

    consul with basic config, common interpolation on server list not yet working

---
+++ consul/plan.sh
@@ -0,0 +1,33 @@
+pkg_origin=starkandwayne
+pkg_name=consul
+pkg_version=0.7.5
+pkg_maintainer='Stark & Wayne beahero@starkandwayne.com'
+pkg_license=("MPL-2.0")
+pkg_description="Consul is a tool for service discovery, monitoring and configuration."
+pkg_upstream_url=https://www.consul.io/
+pkg_source=https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip
+pkg_shasum=40ce7175535551882ecdff21fdd276cef6eaab96be8a8260e0599fadb6f1f5b8
+pkg_filename=${pkg_name}-${pkg_version}_linux_amd64.zip
+pkg_deps=()
+pkg_build_deps=(core/unzip)
+pkg_bin_dirs=(bin)
+pkg_exports=(
+  [port-dns]=ports.dns
+  [port-http]=ports.http
+  [port-rpc]=ports.rpc
+)
+pkg_exposes=(port-dns port-http port-rpc)
+pkg_svc_user=root
+
+do_unpack() {
+  cd "${HAB_CACHE_SRC_PATH}" || exit
+  unzip ${pkg_filename} -d "${pkg_name}-${pkg_version}"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  install -D consul "${pkg_prefix}/bin/consul"
+}
