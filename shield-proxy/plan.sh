da803294a5e40e31efd8d00c34a14a323088d58c
ramonskie <ramonmakkelie@gmail.com>
2017-04-25 16:37:35 +0200

    added shield proxy

---
+++ shield-proxy/plan.sh
@@ -0,0 +1,42 @@
+# This file is the heart of your application's habitat.
+# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
+
+pkg_name=shield-proxy
+pkg_origin=starkandwayne
+pkg_maintainer="Ramon Makkelie <makkelie@starkandwayne.com>"
+pkg_license=('Apache-2.0')
+pkg_version=undefined
+pkg_source=nosuchfile.tar.xz
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(core/nginx core/openssl core/bash)
+
+do_begin() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+update_pkg_version() {
+  return 0
+}
+
+do_download() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
+
+do_prepare() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
