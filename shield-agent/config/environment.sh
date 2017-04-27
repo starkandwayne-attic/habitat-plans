8cdffc15a8d2042fddc50ee7803b3d5cde24b2f5
Justin Carter <justin@starkandwayne.com>
2017-04-27 16:31:32 +0200

    Use https to verify that shield is available from shield-agent

--- shield-agent/config/environment.sh
+++ shield-agent/config/environment.sh
@@ -2,7 +2,7 @@
 
 {{ #if bind.has_daemon }}
 {{ #with bind.daemon.first }}
-SHIELD_ENDPOINT='http://{{sys.ip}}:{{cfg.port}}'
+SHIELD_ENDPOINT='https://{{sys.ip}}:{{cfg.port}}'
 SHIELD_API_TOKEN='{{cfg.provisioning_key}}'
 {{~/with}}
 {{else}}
