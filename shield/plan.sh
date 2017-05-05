3862bcecf2b23196a8c15256fbbd3ea728bcf459
Justin Carter <justin@starkandwayne.com>
2017-05-05 11:43:32 +0200

    Build shield in plan instead of using prebuild binaries

--- shield/plan.sh
+++ shield/plan.sh
@@ -5,10 +5,11 @@ pkg_maintainer="Justin Carter <justin@starkandwayne.com>"
 pkg_license=('MIT')
 pkg_description="A standalone system that can perform backup and restore functions for a wide variety of pluggable data systems."
 pkg_upstream_url="https://github.com/starkandwayne/shield"
-pkg_source="https://github.com/starkandwayne/shield/releases/download/v${pkg_version}/shield-server-linux-amd64.tar.gz"
-pkg_shasum="c70b11d92d18a12bd52a1d8d5907fcb5b386fda6742dff1d184aa63d417b5a5c"
+pkg_source="https://github.com/starkandwayne/shield/archive/v${pkg_version}.tar.gz"
+pkg_shasum="20fafa930d6af51fddcb496f51fc08e7f8d8db3089f940547e95cc0619aca06b"
 
 pkg_deps=(core/bash core/glibc starkandwayne/postgresql starkandwayne/shield-proxy/0.10.4)
+pkg_build_deps=(core/go core/git core/gcc core/make core/gox)
 
 pkg_bin_dirs=(bin)
 
@@ -55,39 +56,41 @@ pkg_binds=(
 # The group to run the service as. The default is hab.
 # pkg_svc_group="$pkg_svc_user"
 
-do_build() {
-  return 0
+do_begin() {
+  export SHIELD_SRC_PATH=${HAB_CACHE_SRC_PATH}/$pkg_dirname/src/github.com/starkandwayne/shield
+  export GOPATH="$HAB_CACHE_SRC_PATH/$pkg_dirname"
 }
 
-# The default implementation is to run make install on the source files and
-# place the compiled binaries or libraries in HAB_CACHE_SRC_PATH/$pkg_dirname,
-# which resolves to a path like /hab/cache/src/packagename-version/. It uses
-# this location because of do_build() using the --prefix option when calling the
-# configure script. You should override this behavior if you need to perform
-# custom installation steps, such as copying files from HAB_CACHE_SRC_PATH to
-# specific directories in your package, or installing pre-built binaries into
-# your package.
-do_install() {
-  cd "${HAB_CACHE_SRC_PATH}/shield-server-linux-amd64" || exit
+do_prepare() {
+  rm -rf $HAB_CACHE_SRC_PATH/shield
+  mkdir -p ${HAB_CACHE_SRC_PATH}/shield
+  mv $HAB_CACHE_SRC_PATH/$pkg_dirname/* $HAB_CACHE_SRC_PATH/shield/
 
+  mkdir -p ${SHIELD_SRC_PATH}
+  mv $HAB_CACHE_SRC_PATH/shield/* ${SHIELD_SRC_PATH}
+
+  export PATH=$PATH:$GOPATH/bin
+
+  git config --global url."git://github.com/".insteadOf "https://github.com/"
+  go get github.com/tools/godep
+  cd ${SHIELD_SRC_PATH}
+  make restore-deps
+}
+
+do_build() {
+  export VERSION=${pkg_version}
+  cd ${SHIELD_SRC_PATH}
+  make release
+}
+
+do_install() {
+  cd ${SHIELD_SRC_PATH}/artifacts
+  tar -xvzf shield-server-linux-amd64.tar.gz
+  cd shield-server-linux-amd64
+
+  mkdir -p ${pkg_prefix}
   cp cli/shield           ${pkg_prefix}/bin
   cp daemon/shieldd       ${pkg_prefix}/bin
   cp daemon/shield-schema ${pkg_prefix}/bin
   cp -R webui             ${pkg_prefix}/webui
-
-  cgo_wrap_binaries
-}
-
-cgo_wrap_binaries() {
-  for binary in ${pkg_prefix}/bin/*; do
-    build_line "Adding wrapper $(basename ${binary}) to $(basename ${binary}.real)"
-    mv ${binary} ${binary}.real
-    cat <<EOF > "${binary}"
-#!$(pkg_path_for bash)/bin/bash
-set -e
-export LD_LIBRARY_PATH="$LD_RUN_PATH"
-exec $(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2 ${binary}.real \$@
-EOF
-    chmod -v 755 "${binary}"
-  done
 }
