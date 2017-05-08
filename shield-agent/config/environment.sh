9e83e196bdabece0bb71b1c5f2b7461a1d1c686c
Justin Carter <justin@starkandwayne.com>
2017-05-08 15:13:46 +0200

    Fix shield-agent using bound daemon

--- shield-agent/config/environment.sh
+++ shield-agent/config/environment.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-{{ #if bind.has_daemon }}
+SHIELD_ENDPOINT='{{cfg.shield_endpoint}}'
+SHIELD_API_TOKEN='{{cfg.provisioning_key}}'
+
 {{ #each bind.daemon.members as |shield| }}
 {{ #unless shield.follower }}
+# Overriding with values found in binding
 SHIELD_ENDPOINT='https://{{shield.sys.ip}}:{{shield.cfg.port}}'
 SHIELD_API_TOKEN='{{shield.cfg.provisioning_key}}'
 {{ /unless }}
 {{~/each}}
-{{else}}
-SHIELD_ENDPOINT='{{cfg.shield_endpoint}}'
-SHIELD_API_TOKEN='{{cfg.provisioning_key}}'
-{{ /if }}
