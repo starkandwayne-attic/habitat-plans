71a8f046c654e474ab8e83175f6b2b7886d150b5
Justin Carter <justin@starkandwayne.com>
2017-05-08 14:33:22 +0200

    Workaround for 0.22 regressions in shield

--- shield-agent/config/environment.sh
+++ shield-agent/config/environment.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 {{ #if bind.has_daemon }}
-{{ #with bind.daemon.first }}
-SHIELD_ENDPOINT='https://{{sys.ip}}:{{cfg.port}}'
-SHIELD_API_TOKEN='{{cfg.provisioning_key}}'
-{{~/with}}
+{{ #each bind.daemon.members as |shield| }}
+{{ #unless shield.follower }}
+SHIELD_ENDPOINT='https://{{shield.sys.ip}}:{{shield.cfg.port}}'
+SHIELD_API_TOKEN='{{shield.cfg.provisioning_key}}'
+{{ /unless }}
+{{~/each}}
 {{else}}
 SHIELD_ENDPOINT='{{cfg.shield_endpoint}}'
 SHIELD_API_TOKEN='{{cfg.provisioning_key}}'
