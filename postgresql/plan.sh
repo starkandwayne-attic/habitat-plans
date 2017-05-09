bf10997199a5cc0086ab0d057fb469fc649218fa
Justin Carter <justin@starkandwayne.com>
2017-05-09 08:49:16 +0200

    Depend on core/postgresql in postgresql plan

--- postgresql/plan.sh
+++ postgresql/plan.sh
@@ -5,25 +5,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="PostgreSQL is a powerful, open source object-relational database system."
 pkg_upstream_url="https://www.postgresql.org/"
 pkg_license=('PostgreSQL')
-pkg_source=https://ftp.postgresql.org/pub/source/v${pkg_version}/${pkg_name}-${pkg_version}.tar.bz2
-pkg_shasum=e5101e0a49141fc12a7018c6dad594694d3a3325f5ab71e93e0e51bd94e51fcd
-pkg_deps=(
-  core/bash
-  core/glibc
-  core/openssl
-  core/perl
-  core/readline
-  core/zlib
-  core/libossp-uuid
-)
-pkg_build_deps=(
-  core/coreutils
-  core/gcc
-  core/make
-)
-pkg_bin_dirs=(bin)
-pkg_include_dirs=(include)
-pkg_lib_dirs=(lib)
+pkg_deps=(core/postgresql/${pkg_version})
 pkg_exports=(
   [port]=port
   [superuser_name]=superuser.name
@@ -31,26 +13,10 @@ pkg_exports=(
 )
 pkg_exposes=(port)
 
-do_prepare() {
-  localedef -i en_US -f UTF-8 en_US.UTF-8
-  export LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-}
-
 do_build() {
-	# ld manpage: "If -rpath is not used when linking an ELF
-	# executable, the contents of the environment variable LD_RUN_PATH
-	# will be used if it is defined"
-	./configure --disable-rpath \
-              --with-openssl \
-              --prefix="$pkg_prefix" \
-              --with-uuid=ossp \
-              --with-includes="$LD_INCLUDE_PATH" \
-              --with-libraries="$LD_LIBRARY_PATH" \
-              --sysconfdir="$pkg_svc_config_path" \
-              --localstatedir="$pkg_svc_var_path"
-	make world
+  return 0
 }
 
 do_install() {
-  make install-world
+  return 0
 }
