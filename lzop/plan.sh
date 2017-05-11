08f5134a1288ba2c3f7d857bf799975902bdcf50
Justin Carter <justin@starkandwayne.com>
2017-05-11 10:39:25 +0200

    Use core/lzop and core/pv

--- lzop/plan.sh
+++
@@ -1,8 +0,0 @@
-pkg_name=lzop
-pkg_origin=starkandwayne
-pkg_version="1.03"
-pkg_source="http://www.lzop.org/download/lzop-${pkg_version}.tar.gz"
-pkg_shasum="c1425b8c77d49f5a679d5a126c90ea6ad99585a55e335a613cae59e909dbb2c9"
-pkg_deps=(core/glibc core/lzo)
-pkg_build_deps=(core/make core/gcc)
-pkg_bin_dirs=(bin)
--- pv/plan.sh
+++
@@ -1,8 +0,0 @@
-pkg_name=pv
-pkg_origin=starkandwayne
-pkg_version="1.6.0"
-pkg_source="https://www.ivarch.com/programs/sources/pv-${pkg_version}.tar.bz2"
-pkg_shasum="0ece824e0da27b384d11d1de371f20cafac465e038041adab57fcf4b5036ef8d"
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/make core/gcc)
-pkg_bin_dirs=(bin)
