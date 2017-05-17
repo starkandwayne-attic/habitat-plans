89a052675a1b710b38c1a8fbc6fa2ebbc8015356
ramonskie <ramonmakkelie@gmail.com>
2017-05-17 16:50:41 +0200

    added haproxy for rabbitmq

---
+++ haproxy/plan.sh
@@ -0,0 +1,43 @@
+pkg_origin=starkandwayne
+pkg_name=haproxy
+pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
+pkg_version=1.6.11
+pkg_maintainer="Ramon Makkelie <makkelie@starkandwayne.com>"
+pkg_license=('GPL-2.0' 'LGPL-2.1')
+pkg_source=http://www.haproxy.org/download/1.6/src/haproxy-${pkg_version}.tar.gz
+pkg_upstream_url="http://git.haproxy.org/git/haproxy-1.6.git/"
+pkg_shasum=62fe982edb102a9f55205792bc14b0d05745cc7993cd6bee5d73cd3c5ae16ace
+pkg_svc_run='haproxy -f config/haproxy.conf -db'
+pkg_exports=(
+  [port]=front-end.port
+  [status-port]=status.port
+)
+pkg_exposes=(port status-port)
+pkg_binds=(
+  [backend]="port"
+)
+pkg_deps=(core/zlib core/pcre core/openssl)
+pkg_build_deps=(
+  core/coreutils
+  core/gcc
+  core/pcre
+  core/make
+  core/openssl
+  core/zlib
+)
+pkg_bin_dirs=(bin)
+
+do_build() {
+  make USE_PCRE=1 \
+       USE_PCRE_JIT=1 \
+       TARGET=linux2628 \
+       USE_OPENSSL=1 \
+       USE_ZLIB=1 \
+       ADDINC="$CFLAGS" \
+       ADDLIB="$LDFLAGS"
+}
+
+do_install() {
+  mkdir -p "$pkg_prefix"/bin
+  cp haproxy "$pkg_prefix"/bin
+}
