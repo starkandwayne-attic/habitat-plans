b896da607fc717fde6cf915d073e2c0e58e39254
Bill Chapman <billchapman@starkandwayne.com>
2017-05-21 17:16:52 -0400

    Varnish plan with proper path to varnishd
    VCL compilation is still erroring

--- varnish/plan.sh
+++ varnish/plan.sh
@@ -7,8 +7,8 @@ pkg_version="5.1.2"
 pkg_source="https://repo.varnish-cache.org/source/${pkg_name}-${pkg_version}.tar.gz"
 
 pkg_shasum="39d858137e26948a7c85f07363f13f0778da61d234126e03a160a0cb9ba4fce3"
-pkg_deps=(core/glibc core/ncurses core/docutils core/gcc)
-pkg_build_deps=(core/make core/python2 core/pkg-config core/pcre core/readline)
+pkg_deps=(core/glibc core/ncurses core/docutils core/pcre core/gcc)
+pkg_build_deps=(core/make core/python2 core/pkg-config core/readline)
 
 pkg_bin_dirs=(sbin)
 pkg_svc_user=(root)
@@ -22,7 +22,8 @@ do_begin() {
 }
 
 do_prepare() {
-  #TODO: We need a sphinx plan to support this
+  #TODO: We need a sphinx plan to support this otherwise we cannot
+  #guarantee idempotency
   pip install sphinx
   return 0
 }
