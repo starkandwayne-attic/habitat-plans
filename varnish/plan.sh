a808ef6333e5930b1ac8d660ad4a97029856fe37
ramonskie <ramonmakkelie@gmail.com>
2017-05-22 18:27:09 +0200

    varnish compiling tryouts

--- varnish/plan.sh
+++ varnish/plan.sh
@@ -7,8 +7,8 @@ pkg_version="5.1.2"
 pkg_source="https://repo.varnish-cache.org/source/${pkg_name}-${pkg_version}.tar.gz"
 
 pkg_shasum="39d858137e26948a7c85f07363f13f0778da61d234126e03a160a0cb9ba4fce3"
-pkg_deps=(core/glibc core/ncurses core/docutils core/pcre core/gcc)
-pkg_build_deps=(core/make core/python2 core/pkg-config core/readline)
+pkg_deps=(core/glibc core/ncurses core/docutils core/pcre core/gcc core/bash core/gcc-libs core/gettext core/groff core/libbsd core/libedit core/libpipeline core/libunistring core/man-db )
+pkg_build_deps=(core/make core/python2 core/pkg-config core/readline core/bash core/graphviz core/libtool core/libedit core/automake core/m4 core/autoconf)
 
 pkg_bin_dirs=(sbin)
 pkg_svc_user=(root)
@@ -26,6 +26,15 @@ do_prepare() {
   #guarantee idempotency
   pip install sphinx
   return 0
+}
+
+do_build() {
+  # TODO: if we don't copy this aclocal will fail. need to figure out how to fix this
+  cp $(pkg_path_for core/pkg-config)/share/aclocal/pkg.m4 $(pkg_path_for core/automake)/share/aclocal/
+  sh autogen.sh
+  sh configure --prefix=$pkg_prefix
+  make
+  make check
 }
 
 do_check() {
