551ed2b58b83a8c16e308450f4ae17a62872531c
Justin Carter <justin@starkandwayne.com>
2017-08-17 11:12:42 +0200

    Update pg

--- postgresql/plan.sh
+++ postgresql/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=postgresql
-pkg_version=9.6.1
+pkg_version=9.6.3
 pkg_origin=starkandwayne
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="PostgreSQL is a powerful, open source object-relational database system."
@@ -8,7 +8,7 @@ pkg_license=('PostgreSQL')
 pkg_source=https://ftp.postgresql.org/pub/source/v${pkg_version}/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=e5101e0a49141fc12a7018c6dad594694d3a3325f5ab71e93e0e51bd94e51fcd
 pkg_deps=(
-  starkandwayne/wal-e
+  core/wal-e
   core/envdir
   core/postgresql/${pkg_version}
   core/bash
